2e061da88fbeb38967d197dd84dc814f43acc2d3
Alireza Sanaee <sarsanaee@gmail.com>
2023-03-24 16:54:24 -0500

    Update solvers.sh

--- .travis/solvers.sh
+++ .travis/solvers.sh
@@ -22,7 +22,9 @@ for solver in ${SOLVER_LIST}; do
     # can go to a file
     git clone https://github.com/sarsanaee/z3.git
     cd z3
-    git checkout z3-4.3.1-updated
+    git checkout z3-4.3.1
+    autconf
+    ./configure
     python2.7 scripts/mk_make.py
     cd build
     make
