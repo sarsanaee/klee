ba76d337b01c748f6af73ba630c16f37430a6f9e
Alireza Sanaee <sarsanaee@gmail.com>
2023-03-24 15:26:22 -0500

    Update solvers.sh

--- .travis/solvers.sh
+++ .travis/solvers.sh
@@ -18,7 +18,7 @@ for solver in ${SOLVER_LIST}; do
   Z3)
     echo "Z3"
     # Should we install libz3-dbg too?
-    sudo apt-get -y install libz3 libz3-dev
+    sudo apt-get -y install libz3-4 libz3-dev
     ;;
   metaSMT)
     echo "metaSMT"
